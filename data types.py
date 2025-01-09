378c3579f4002f3df1fd442faa5fc2efd90a12bf
Abir <abirgpx@gmail.com>
2025-01-09 20:47:50 +0600

    first commit

---
+++ data types.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print(f"{name}: {type(name)}")
+
+age = int(input("Enter your Age: "))
+print(f"{age}: {type(age)}")
+
+weight = float(input("Enter your Weight: "))
+print(f"{weight}: {type(weight)}")
+
+isStudent = bool(input("Are you a Student [True/Falseabir]: "))
+print(f"{isStudent}: {type(isStudent)}")
---
+++ string operation.py
@@ -0,0 +1,22 @@
+word = input("Enter a Word: ")
+
+print(word.upper())
+print(word.lower())
+print(word.capitalize())
+
+revWord = word[::-1]
+
+print(revWord)
+
+print()
+print()
+
+msg = input("Enter your Congratulations Message : ")
+
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+
+revMsg = msg[::-1]
+
+print(f"\n\nYour Reveresed Word : {revMsg}")
